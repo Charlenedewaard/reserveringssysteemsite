36212fdaf90cfb7d8dc2940f67e57f941e51f67c
Charlene de Waard <0942584@hr.nl>
2018-06-18 10:17:23 +0200

    edit responsive properties

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include_once 'config.php';
 <section>
     <div class="row">
         <div class="col push-s1">
-            <img src="campingplek1.jpg" alt="camping plek 2" class="col s5" width="500"  />
+            <img src="campingplek1.jpg" alt="camping plek 2" class="col s12 m5"  >
             <div class="col m3">
                 <h2>Oost west Oost best</h2>
                 <p>Een mooie campingplek in het oosten! Deze campingplek is geschikt voor elke campingganger. Hier kan u zowel met camper of tent staan. Deze plek ligt
@@ -35,7 +35,7 @@ include_once 'config.php';
 
     <div class="row">
         <div class="col push-s1">
-            <img src="campingplek2.jpg" alt="camping plek 2" class="col s5" width="500">
+            <img src="campingplek2.jpg" alt="camping plek 2" class="col s5" >
             <div class="col m3">
                 <h2>Wild West</h2>
                 <p>Een leuke plek voor gezinnen met jongere kinderen! Deze campingplek is inclusief een kinderspeelparkje. Het Woeste Water Zwembad
@@ -46,7 +46,7 @@ include_once 'config.php';
     </div>
     <div class="row">
         <div class="col push-s1">
-            <img src="campingplek3.jpg" alt="camping plek 3" class="col s5" width="500">
+            <img src="campingplek3.jpg" alt="camping plek 3" class="col s5" >
             <div class="col m3">
                 <h2>South Side</h2>
                 <p>Voor de echte vakantiegenieter die graag nog van een beetje privacy houdt. Deze locatie is voorzien van heggen tussen
