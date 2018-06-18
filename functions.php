808f4f10da270b2fc845f486fba1129a6ee33f75
Charlene de Waard <0942584@hr.nl>
2018-06-18 23:48:24 +0200

    added security sql string

--- functions.php
+++ functions.php
@@ -21,10 +21,10 @@ function register(){
 
     // receive all input values from the form. Call the e() function
     // defined below to escape form values
-    $username    =  e($_POST['username']);
-    $email       =  e($_POST['email']);
-    $password_1  =  e($_POST['password_1']);
-    $password_2  =  e($_POST['password_2']);
+    $username    =  mysqli_real_escape_string($conn1, $_POST['username']);
+    $email       =  mysqli_real_escape_string($conn1,$_POST['email']);
+    $password_1  =  mysqli_real_escape_string($conn1,$_POST['password_1']);
+    $password_2  =  mysqli_real_escape_string($conn1,$_POST['password_2']);
 
     // form validation: ensure that the form is correctly filled
     if (empty($username)) {
@@ -40,9 +40,9 @@ function register(){
         array_push($errors, "The two passwords do not match");
     }
 
-    // register user if there are no errors in the form
+    // regristreerd nieuwe user als er geen error is
     if (count($errors) == 0) {
-        $password = md5($password_1);//encrypt the password before saving in the database
+        $password = md5($password_1);// hashed
 
         if (isset($_POST['user_type'])) {
             $user_type = e($_POST['user_type']);
